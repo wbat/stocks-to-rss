f567dfdab3d047dec3b4758dd7d60092314cef2e
bateller <brianateller@gmail.com>
2015-06-18 16:31:32 -0400

    Added RSS-2.0-Generation-Class Phackagist Dependency

--- tests/test.php
+++ tests/test.php
@@ -4,6 +4,28 @@ require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Compos
 
 use StockstoRSS\getStock;
 
-echo getStock::stock();
+// echo getStock::stock();
 
+use RSS_Generation_Class\rss;
+
+// echo rss::rss();
+
+$rss = new rss('utf-8');
+$rss->channel('Signera Stocks Feed', 'http://www.signera.net', 'Live stocks');
+$rss->language('en-us');
+$rss->copyright('Copyright by Signera 2013-'. date('Y'));
+$rss->managingEditor('support@signera.net');
+$rss->category('');
+
+echo $rss->startRSS();
+
+$rss->itemTitle('Test RSS Title');
+$rss->itemLink('');
+$rss->itemDescription('');
+$rss->itemAuthor('support@signera.net');
+$rss->itemGuid(time());
+$rss->itemSource('Signera', 'n/a');
+$rss->addItem();
+
+echo $rss->RSSdone2();
 ?>
